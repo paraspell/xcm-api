6a0553691cae4d994d250aa0c077a44e747e1101
Michael Absolon <michaelabsolon@Michaels-MacBook-Air.local>
2023-07-15 20:03:28 +0200

    Add endpoints for HRMP pallet (Opening and closing channels)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { XTransferModule } from './x-transfer/x-transfer.module';
 import { AssetsModule } from './assets/assets.module';
+import { ChannelsModule } from './channels/channels.module';
 
 @Module({
-  imports: [XTransferModule, AssetsModule],
+  imports: [XTransferModule, AssetsModule, ChannelsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,23 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import { TNode, getNodeEndpointOption } from '@paraspell/sdk';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+
 export const isNumeric = (num: any) => !isNaN(num);
+
+export const createApiInstance = async (wsUrl: string) => {
+  const wsProvider = new WsProvider(wsUrl);
+  return await ApiPromise.create({ provider: wsProvider });
+};
+
+export const findWsUrlByNode = (node: TNode) => {
+  const nodeInfo = getNodeEndpointOption(node);
+  const providers = Object.values(nodeInfo.providers);
+
+  if (providers.length < 1) {
+    throw new InternalServerErrorException(
+      `We couldn't find any WS url for node ${node}. Check your @polkadot/apps-config dependency version or open an issue here https://github.com/paraspell/xcm-api`,
+    );
+  }
+
+  return providers[0];
+};
