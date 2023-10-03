fc52134aa45781275bc600af79ebae6c3a688bb7
Rikkard29 <absolon.mm@gmail.com>
2023-10-03 19:26:22 +0200

    Add Sentry error tracking 🧰

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,23 @@
-import { Module } from '@nestjs/common';
+import { HttpException, HttpStatus, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { XTransferModule } from './x-transfer/x-transfer.module';
 import { AssetsModule } from './assets/assets.module';
 import { ChannelsModule } from './channels/channels.module';
 import { PalletsModule } from './pallets/pallets.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { AuthGuard } from './auth/auth.guard';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
 import { UsersService } from './users/users.service';
 import { AnalyticsModule } from './analytics/analytics.module';
+import { SentryInterceptor, SentryModule } from '@ntegral/nestjs-sentry';
+import { sentryConfig } from './config/sentry.config';
+import { typeOrmConfig } from './config/typeorm.config';
+import { throttlerConfig } from './config/throttler.config';
 
 @Module({
   imports: [
@@ -27,38 +30,19 @@ import { AnalyticsModule } from './analytics/analytics.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DB_HOST'),
-        port: config.get('DB_PORT'),
-        username: config.get('DB_USER'),
-        password: config.get('DB_PASS'),
-        database: config.get('DB_NAME'),
-        entities: [User],
-        synchronize: true,
-      }),
+      useFactory: typeOrmConfig,
     }),
     ThrottlerModule.forRootAsync({
       inject: [ConfigService, UsersService],
-      useFactory: (config: ConfigService) => ({
-        ttl:
-          process.env.NODE_ENV === 'test'
-            ? 0
-            : config.get('RATE_LIMIT_TTL_SEC'),
-        limit: (context) => {
-          const request = context.switchToHttp().getRequest();
-          if (request.user && request.user.requestLimit) {
-            return request.user.requestLimit;
-          }
-          return request.user
-            ? config.get('RATE_LIMIT_REQ_COUNT_AUTH')
-            : config.get('RATE_LIMIT_REQ_COUNT_PUBLIC');
-        },
-      }),
+      useFactory: throttlerConfig,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
       serveRoot: '/app',
+    }),
+    SentryModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: sentryConfig,
     }),
   ],
   controllers: [AppController],
@@ -70,6 +54,19 @@ import { AnalyticsModule } from './analytics/analytics.module';
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useFactory: () =>
+        new SentryInterceptor({
+          filters: [
+            {
+              type: HttpException,
+              filter: (exception: HttpException) =>
+                HttpStatus.INTERNAL_SERVER_ERROR > exception.getStatus(),
+            },
+          ],
+        }),
     },
   ],
 })
