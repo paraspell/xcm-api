88b9de9fe66b58d3fe99dac1d848242333445888
Rikkard29 <absolon.mm@gmail.com>
2023-11-16 10:54:38 +0100

    Add endpoint for Sentry testing 🪲

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,4 +6,12 @@ export class AppController {
   @Redirect('https://github.com/paraspell/xcm-api', 301)
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   root() {}
+
+  @Get('sentry-test')
+  sentryTest() {
+    if (process.env.NODE_ENV === 'production') {
+      return 'Sentry test is only available in development mode.';
+    }
+    throw new Error('Sentry test');
+  }
 }
